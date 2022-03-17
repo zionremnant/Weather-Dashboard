ec5645f2dbe6e9ba644a57758863a903fc470ade
Zion Yang <zionremnant@gmail.com>
2022-03-17 00:53:39 -0700

    linked bootstraps

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var key = "";
-var city = "";
+var key = "68cc41e37ba870c81cf19b09d5d0a89b";
+var city = "Los Angeles";
 
 //Grabs the current time and date
 var date = moment().format("dddd, MMMM Do YYYY");
