44b74d92536ee1fc16c213e62cbb6dcc9c04a04d
Zion Yang <zionremnant@gmail.com>
2022-03-17 20:02:34 -0700

    seach history js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var key = "68cc41e37ba870c81cf19b09d5d0a89b";
 var city = "Los Angeles";
 
-//Grabs the current time and date
+//current time/date
 var date = moment().format("dddd, MMMM Do YYYY");
 var dateTime = moment().format("YYYY-MM-DD HH:MM:SS");
+
+// search history
+var searchHistory = [];
+$(".search").on("click", function(event){
+    event.preventDefault();
+    city = $(this).parent(".btnPar").siblings(".textVal").value().trim();
+    if(city===""){
+        return;
+    }
+    searchHistory.push(city);
+    localStorage.setItem("city", JSON.stringify(searchHistory));
+    fiveDay.empty();
+    history();
+});
+// search history buttons
+var 
