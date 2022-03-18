2d1e2ea9aac9581c9bd9bd9a79440a132b133fe4
Zion Yang <zionremnant@gmail.com>
2022-03-17 20:12:57 -0700

    search history btn js

--- script.js
+++ script.js
@@ -7,16 +7,36 @@ var dateTime = moment().format("YYYY-MM-DD HH:MM:SS");
 
 // search history
 var searchHistory = [];
-$(".search").on("click", function(event){
-    event.preventDefault();
-    city = $(this).parent(".btnPar").siblings(".textVal").value().trim();
-    if(city===""){
-        return;
-    }
-    searchHistory.push(city);
-    localStorage.setItem("city", JSON.stringify(searchHistory));
-    fiveDay.empty();
-    history();
+$(".search").on("click", function (event) {
+  event.preventDefault();
+  city = $(this).parent(".btnPar").siblings(".textVal").value().trim();
+  if (city === "") {
+    return;
+  }
+  searchHistory.push(city);
+  localStorage.setItem("city", JSON.stringify(searchHistory));
+  fiveDay.empty();
+  history();
 });
 // search history buttons
-var 
+var historyEl = $(".searchHistory");
+function history() {
+  historyEl.empty();
+
+  for (let i = 0; i < searchHistory.length; i++) {
+    var btnEl = $("<button>").text(`${searchHistory[i]}`);
+    var rowEl = $("<row>");
+
+    rowEl.addClass("row historyRowBtn");
+    btnEl.addClass("btn btn-outline-secondary historyBtn");
+    btnEl.attr("type", "button");
+
+    historyEl.prepend(rowEl);
+    rowEl.append(btnEl);
+  }
+  if (!city) {
+    return;
+  }
+  // start searching btn
+  $(".historyBtn").on("click", function (event) {});
+}
